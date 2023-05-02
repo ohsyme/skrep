712f1a3602fce3b90354be0d9502db27393de9ea
ohsyme <gamingrkp@gmail.com>
2023-05-02 17:27:32 +0700

    add nh

--- test/index.js
+++ test/index.js
@@ -1,9 +1,7 @@
 (async () => {
     const api = require('skrep');
-     const url = 'https://212.32.226.234/the-pink-album-chapter-01/';
-     const result = await api.nsfw.doujindesu.download(url);
-    //const url = 'https://www.facebook.com/reel/1221811732075864/?mibextid=eA6d3HrSeJoD5Dg0'
-    // const result1 = await api.downloader.Facebook.scrape(url);
+     const url = '406591';
+     const result = await api.nsfw.nhentai.scrape(url);
     console.log(result)
 })();
       
