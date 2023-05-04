84cea48e1b99c2e62088aaa5abe5d833f67404a3
ohsyme <gamingrkp@gmail.com>
2023-05-04 15:53:57 +0700

    add tiktok

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 (async () => {
     const api = require('skrep');
-     const url = 'https://www.hentaibatch.com/2022/09/kanpeki-ojousama-no-watakushi-ga-dogeza.html';
-     const result = await api.nsfw.HentaiBatch.detail(url);
+     const url = 'https://www.tiktok.com/@hanru01/video/7224425974945942786?is_from_webapp=1&sender_device=pc&web_id=7228471037720151554';
+     const result = await api.downloader.tiktok.download(url);
      console.log(result)
 })();
       
